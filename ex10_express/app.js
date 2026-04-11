fce4e9786eec53bb7d3e65a9c0b13f885def996f
Julia <julia_es@icloud.com>
2026-04-11 14:53:55 +0300

    add ex10

--- ex10_express/app.js
+++ ex10_express/app.js
@@ -1,22 +1,25 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
-
-
-
-
-import {dirname, join} from "node:path";
+import {fileURLToPath} from 'node:url';
+import {dirname, join} from 'node:path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-import booksRouter from './routes/books.js';
-import userRouter from './routes/users.js';
+import indexRouter from './routes/index.js';
+import usersRouter from './routes/users.js';
 
 const app = express();
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(join(__dirname, 'public')));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-
-
-
-
-
+export default app;
--- ex10_express/controllers/books.js
+++
@@ -1,8 +0,0 @@
-class BooksController {
-    update = (req, res,next) => {
-        res.send('patch/put books by id');
-    }
-}
-
-const ctrl = new BooksController();
-export default ctrl
